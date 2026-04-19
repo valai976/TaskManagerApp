f85afe69a0482b2926d3fe3425356054826de435
Valai Willie <willievalai@gmail.com>
2026-04-19 18:20:04 -0400

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApp.Models;
+
+namespace TaskManagerApp.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagerApp.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
