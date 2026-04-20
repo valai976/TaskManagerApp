05cff08a94ec3db8a1cbf0175643e46c50fe2a05
Valai Willie <willievalai@gmail.com>
2026-04-19 20:41:24 -0400

    I

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public ProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+            return View();
+        }
+    }
+}
