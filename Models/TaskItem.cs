19a82f0e5354885e7ae104c22b0ec36e355213fe
Valai Willie <willievalai@gmail.com>
2026-04-20 15:12:25 -0400

    4

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,11 +1,15 @@
-﻿namespace TaskManagerApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.Models
 {
     public class TaskItem
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Title is needed")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Description is needed")]
         public string Description { get; set; }
 
         public bool IsCompleted { get; set; }
